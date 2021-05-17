4436a7ec769680666890c3e93ddfb3621fc9f4f4
shamrice <shamrice@gmail.com>
2021-05-17 00:58:30 -0400

    Adding header text to build_all build script.

--- src/build_all.sh
+++ src/build_all.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo ------------------------------------------------------
+echo  COBOL Roguelike Main Build Script
+echo  By: Erik Eriksen 
+echo  https://github.com/shamrice/COBOL-Roguelike
+echo ------------------------------------------------------
+echo 
+echo Calling all project build scripts...
+echo 
+
 ./build_item_creator.sh
 ./build_editor.sh
 ./build_engine.sh
